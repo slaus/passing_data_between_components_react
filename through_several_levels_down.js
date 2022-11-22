29aa40c81a2c4169eb848edb16873ab323d8c796
Vyacheslav <mister.slaus@gmail.com>
2022-11-22 13:32:58 +0200

    Update through_several_levels_down.js

--- through_several_levels_down.js
+++ through_several_levels_down.js
@@ -1,1 +1,33 @@
+import { useState } from 'react'
 
+const Parent = () => {
+  const [value, setValue] = useState('')
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return (
+    <div>
+       <input
+         type="text"
+         onChange={handleChange }
+       />
+      {/* передаем проп в дочерний компонент */}
+      <Child_1 value={value} />
+    </div>
+  )
+}
+
+const Child_1 = ({ value }) => {
+  return (
+    {/* передаем проп в дочерний компонент */}
+    <Child_2 value={value} />
+  )
+}
+
+const Child_2 = ({ value }) => {
+  return (
+    <span>Value is: {value || '<Not set>'}</span>
+  )
+}
